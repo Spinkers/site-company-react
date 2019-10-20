a5a02b353fd7ccb74644bae11059686574c868ca
Spinkers <lucasmouraolopes@gmail.com>
2019-10-20 14:47:37 -0300

    Adicionando componentes ao app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Site from './components/navegacao';
+import Rodape from './components/rodape/index';
+import Contato from './components/contato/index';
+import Precos from './components/precos/index';
+import Portifolio from './components/portifolio/index';
+import Servicos from './components/servicos/index';
+import Sobre from './components/sobre/index';
+import Jumbotron from './components/jumbotron/index';
+import Navegacao from './components/navegacao/index';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navegacao />
+      <Jumbotron />
+      <Sobre />
+      <Servicos />
+      <Portifolio />
+      <Precos />
+      <Contato />
+      <Rodape />
     </div>
   );
 }
